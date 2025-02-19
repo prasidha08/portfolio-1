3aafbd5b25dd9d33d3885290dc6ec8433f2ebe52
Prasidha Acharya <acharyapraidha08>
2025-02-19 18:31:11 +0545

    header while scrolling down

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function openMenu() {
-
   // Select the element with the class name "mobile-menu-nav-bar" to change its style
   const arrayOfUl = document.getElementsByClassName("mobile-menu-nav-bar");
 
@@ -8,7 +7,8 @@ function openMenu() {
   const mobileMenuNavbar = arrayOfUl[0];
 
   // Select the element with the class name "mobile-view-nav" to change its style
-  const mainMobileViewHeader = document.getElementsByClassName("mobile-view-nav");
+  const mainMobileViewHeader =
+    document.getElementsByClassName("mobile-view-nav");
 
   // The result of mainMobileViewHeader is an array-like object
   // Access the first element of mainMobileViewHeader
@@ -17,12 +17,34 @@ function openMenu() {
   // Check the condition to toggle the menu options
   // If the display is "none" or undefined, update the display to "flex" to show the menu options
   // If the display is "flex", update the display to "none" to hide the menu options
-  if (!mobileMenuNavbar.style.display || mobileMenuNavbar.style.display === "none") {
+  if (
+    !mobileMenuNavbar.style.display ||
+    mobileMenuNavbar.style.display === "none"
+  ) {
     mobileMenuNavbar.style.display = "flex";
-    mainMobileViewObj.style.height = '450px';
+    mainMobileViewObj.style.height = "450px";
   } else {
     mobileMenuNavbar.style.display = "none";
-    mainMobileViewObj.style.height = '70px';
+    mainMobileViewObj.style.height = "70px";
   }
-  
 }
+
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+
+  const browerMenuHeader = document.getElementsByClassName("menu-nav-bar")[0];
+
+  if (scrollY >= 357) {
+    // console.log("TCL: scrollY", scrollY);
+    browerMenuHeader.style.position = "fixed";
+    browerMenuHeader.style.width = "100%";
+    browerMenuHeader.style.backgroundColor = "white";
+    browerMenuHeader.style.padding = "2rem";
+  } else {
+    browerMenuHeader.style.position = "";
+    browerMenuHeader.style.backgroundColor = "";
+    browerMenuHeader.style.width = "";
+    browerMenuHeader.style.padding = "4rem";
+  }
+});
